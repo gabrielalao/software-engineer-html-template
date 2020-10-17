92f44ab30f7b220b586ca170e54a72d749def64d
Gabriel Alao <gabrieldeveloper1@gmail.com>
2020-10-17 03:30:59 +0100

    major update

---
+++ assets/js/Media-Slider-Bootstrap-3.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+    $('#Carousel').carousel({
+        interval: 5000
+    })
+});
+
+
+$('.brand-slider').owlCarousel({
+    loop: true,
+    dots: false,
+    nav: true,
+    navText: ['',''],
+    margin: 120,
+    responsive: {
+        1200:{
+            items:5
+        },
+        970:{
+            items:4
+        },
+        768:{
+            items:3,
+			nav: false,
+        },
+        480:{
+            items:2,
+			nav: false,
+        },
+        0:{
+            items:2,
+			nav: false,
+			margin: 40,
+        },
+    }
+})
+
+
+
+
+
+
+
+
+
+
